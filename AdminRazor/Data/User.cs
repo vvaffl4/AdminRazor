9f4b8c03004ece5dee2f4cb3fa0cb1283bc67245
PW\pw <patrickwobben@live.nl>
2022-09-27 21:27:19 +0200

    Validation for creating users

--- AdminRazor/Data/User.cs
+++ AdminRazor/Data/User.cs
@@ -7,6 +7,8 @@ namespace RazorAdmin.Data
 	public class User
 	{
 		[Required(ErrorMessage = "A username is required")]
+		[MinLength(3, ErrorMessage = "Your username must be longer than 2 characters.")]
+		[MaxLength(16, ErrorMessage = "Your username can't be longer than 16 characters.")]
 		[RegularExpression("^[a-zA-Z0-9_]*$", ErrorMessage = "Usernames can only contain letters, numbers and _")]
 		public string UserName { get; set; }
 		[Required(ErrorMessage = "A password is required")]
@@ -20,7 +22,7 @@ namespace RazorAdmin.Data
 		// Example User data to show in this example (instead of Database data)
 		public static IEnumerable<User> Users = new User[]
 		{
-			new User{ UserName = "Admin", Password = "Admin!" },
+			new User{ UserName = "Admin", Password = "ThisIsAnAdmin1!" },
 			new User { UserName = "User1", Password = "User1!" },
 			new User { UserName = "User2", Password = "User2!" },
 			new User { UserName = "User3", Password = "User3!" },
--- AdminRazor/Pages/Create.cshtml.cs
+++ AdminRazor/Pages/Create.cshtml.cs
@@ -7,6 +7,9 @@ namespace RazorAdmin.Pages
 {
 	public class CreateModel : PageModel
 	{
+		[ViewData]
+		public string ErrorMessages { get; set; }
+
 		[BindProperty]
 		public User User { get; set; }
 
@@ -15,8 +18,23 @@ namespace RazorAdmin.Pages
 
 		public IActionResult OnPost()
 		{
-			// Redirect to Razor Page "Accounts" with the Handler "Create", and parameters "username" and "password"
-			return RedirectToPage("Accounts", "Create", new { username = User.UserName, password = User.Password });
+			// Checks if data annotations on models are valid.
+			// Can be used to prevent SQL injection
+			if (ModelState.IsValid)
+			{
+				// Redirect to Razor Page "Accounts" with the Handler "Create", and parameters "username" and "password"
+				return RedirectToPage("Accounts", "Create", new { username = User.UserName, password = User.Password });
+			}
+
+			// Set ViewData "ErrorMessages" to all errors encountered in the ModelState (aka data notations)
+			ErrorMessages = string.Join(
+				" \n ",
+				ModelState.Values
+					.SelectMany(v => v.Errors)
+					.Select(error => error.ErrorMessage));
+
+			// Return to same page with ErrorMessages
+			return Page();
 		}
 	}
 }
